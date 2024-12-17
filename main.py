d3c181053822112bef54d22d90ee8826f05c6d02
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2024-12-17 01:43:05 +0000

    Added pydantic and started using a simple model

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask import request
 
+from pydantic import BaseModel
+
 app = Flask(__name__)
+
+class Email(BaseModel):
+    f: str
+    s: str
+
 
 @app.route('/email', methods=['POST'])
 def login():
     data_received = request.get_json()
     
-    f = data_received['envelope']['from']
-    s = data_received['headers']['subject']
+    email_received = Email(
+        f = data_received['envelope']['from'],
+        s = data_received['headers']['subject']
+    )
 
-    return f"Message from {f} about {s}"
+    return f"received: {email_received}"
