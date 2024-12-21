4f1fb0ce32f3ad1da57ae29f12f0d2bb1f2f7e91
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2024-12-21 20:01:41 +0100

    Cleaned some of the database code from Flask tutorial

--- cloudmailin/__init__.py
+++ cloudmailin/__init__.py
@@ -7,18 +7,12 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "emails.sqlite"),
     )
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     from . import db
 
@@ -27,9 +21,5 @@ def create_app(test_config=None):
     from . import generic
 
     app.register_blueprint(generic.bp)
-
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
 
     return app
