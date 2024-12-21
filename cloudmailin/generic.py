7c8fad55412daf207c3a364054a659bfa30f9608
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2024-12-21 22:13:48 +0100

    Separated schema and using pydantic for data validation

--- cloudmailin/generic.py
+++ cloudmailin/generic.py
@@ -1,18 +1,24 @@
-from flask import Blueprint
-from flask import request
-from flask import jsonify
+from flask import Blueprint, request, jsonify
+from pydantic import ValidationError
+
+from cloudmailin.schemas import Email
 
 bp = Blueprint("generic", __name__, url_prefix="/generic")
 
 
 @bp.route("/new", methods=["POST"])
 def new_generic_email():
-    data_received = request.get_json()
+    try:
+        data_received = request.get_json()
 
-    email_received = {
-        "sender": data_received["envelope"]["from"],
-        "recipient": data_received["envelope"]["to"],
-        "subject": data_received["headers"]["subject"],
-    }
+        email = Email(**data_received)
 
-    return jsonify(email_received)
+        return jsonify({
+            "sender":email.sender,
+            "recipient":email.recipient,
+            "subject":email.subject
+        }),200
+
+    except ValidationError as e:
+        return jsonify({"error": e.errors()}), 400
+
