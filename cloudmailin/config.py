c605f45ef53a65ea09d029e2687a9783560c47d8
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2025-01-28 16:22:33 +0000

    Adapted files to comply with Black

--- cloudmailin/config.py
+++ cloudmailin/config.py
@@ -1,16 +1,19 @@
 class Config:
     pass
 
+
 class ProductionConfig(Config):
     FIRESTORE_COLLECTION = "emails"
+
 
 class StagingConfig(Config):
     FIRESTORE_COLLECTION = "staging_emails"
 
+
 class UnitTestingConfig(Config):
     FIRESTORE_COLLECTION = None
 
-class FunctionalTestingConfig(Config):
-#    FIRESTORE_COLLECTION = "functional_test_emails"
-    FIRESTORE_COLLECTION = None
 
+class FunctionalTestingConfig(Config):
+    #    FIRESTORE_COLLECTION = "functional_test_emails"
+    FIRESTORE_COLLECTION = None
