df0008b03bddb74e232cfa62d699b5b2e4a1b60f
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2025-01-28 16:20:01 +0000

    Changed name of production collection

--- cloudmailin/config.py
+++ cloudmailin/config.py
@@ -1,27 +1,16 @@
 class Config:
     pass
 
-
 class ProductionConfig(Config):
-    FIRESTORE_COLLECTION = "production_emails"
-
+    FIRESTORE_COLLECTION = "emails"
 
 class StagingConfig(Config):
     FIRESTORE_COLLECTION = "staging_emails"
 
-
-#    DEBUG = True
-
-
 class UnitTestingConfig(Config):
-    #    TESTING = True
     FIRESTORE_COLLECTION = None
 
-
 class FunctionalTestingConfig(Config):
-    #    TESTING = True
-    #    FUNCTIONAL_TESTING = True
-    FIRESTORE_COLLECTION = "functional_test_emails"
+#    FIRESTORE_COLLECTION = "functional_test_emails"
+    FIRESTORE_COLLECTION = None
 
-
-#    DEBUG = False
